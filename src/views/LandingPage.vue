f62fc5d3f33a2066169d26fe7478c8bb8d1a4ef7
frankPIF <ubayfranklou@gmail.com>
2022-11-25 14:01:11 +0800

    Added a form.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,67 +2,54 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Landing Page</ion-title>
+        <ion-title>Landing</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Landing Page</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <div id="container">
-        <h1>Welcome!</h1>
-        <ion-button router-link="/about">About Page</ion-button>
-        <ion-button @click="$router.push({name: 'SignUp'})">Sign Up</ion-button>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="12">
+            <ion-img :src="bgImage"></ion-img>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col offset="2" size="2">
+            <ion-button color="dark" size="small" fill="outline"  router-link="/about">Sign In</ion-button>
+          </ion-col>
+          <ion-col offset="3" size="2">
+            <ion-button color="medium" size="small" fill="outline" @click="$router.push({ name: 'SignUp' })">Sign Up</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col offset="2" size="8">
+            <ion-datetime></ion-datetime>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonImg, IonButton, IonContent, IonPage, IonDatetime } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import bg from '../assets/imgs/bg-img.png';
 
 export default defineComponent({
   name: 'HomePage',
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar,
-    IonButton
+    IonButton,
+    IonDatetime,
+    IonImg
   },
+  data() {
+    return {
+      bgImage: bg
+    }
+  }
 });
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,66 +1,146 @@
 <template>
     <ion-page>
-      <ion-header :translucent="true">
-        <ion-toolbar>
-          <ion-title>Sign Up</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">Sign Up</ion-title>
-          </ion-toolbar>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>Sign Up</ion-title>
+            </ion-toolbar>
         </ion-header>
-        <div id="container">
-          <h1>Welcome!</h1>
-        </div>
-      </ion-content>
+        <ion-content :fullscreen="true">
+            <ion-grid>
+                <ion-row>
+                    <ion-col offset="3" size="6" style="text-align:center">
+                        <h1>Membership Form</h1>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col offset="2" size="4">
+                        <ion-item>
+                            <ion-label position="floating">First Name</ion-label>
+                            <ion-input></ion-input>
+                        </ion-item>
+                    </ion-col>
+                    <ion-col offset="1" size="4">
+                        <ion-item>
+                            <ion-label position="floating">Last Name</ion-label>
+                            <ion-input></ion-input>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col offset="2" size="9">
+                        <ion-item>
+                            <ion-label position="floating">Address</ion-label>
+                            <ion-textarea :auto-grow="true"></ion-textarea>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col offset="2" size="4">
+                        <ion-item>
+                            <ion-label position="floating">Email</ion-label>
+                            <ion-input></ion-input>
+                        </ion-item>
+                    </ion-col>
+                    <ion-col offset="1" size="4">
+                        <ion-item>
+                            <ion-label position="floating">Password</ion-label>
+                            <ion-input type="password"></ion-input>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col offset="2" size="4">
+                        <ion-label>Sex</ion-label>
+                        <ion-radio-group>
+                            <ion-item>
+                                <ion-label>Male</ion-label>
+                                <ion-radio slot="end" value="Male"></ion-radio>
+                            </ion-item>
+                            <ion-item>
+                                <ion-label>Female</ion-label>
+                                <ion-radio slot="end" value="Female"></ion-radio>
+                            </ion-item>
+                        </ion-radio-group>
+                    </ion-col>
+                    <ion-col offset="1" size="4">
+                        <ion-label>Available Government ID</ion-label>
+                        <ion-item>
+                            <ion-select placeholder="Select all fruits that apply" :multiple="true">
+                                <ion-select-option value="National ID">National ID</ion-select-option>
+                                <ion-select-option value="Driver's License">Driver's License</ion-select-option>
+                                <ion-select-option value="Passport">Passport</ion-select-option>
+                            </ion-select>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col offset="2" size="9">
+                        <ion-item>
+                            <ion-label>Date of Birth</ion-label>
+                            <ion-datetime-button datetime="datetime"></ion-datetime-button>
+                            <ion-modal :keep-contents-mounted="true">
+                                <ion-datetime id="datetime"></ion-datetime>
+                            </ion-modal>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+        </ion-content>
     </ion-page>
-  </template>
+</template>
   
-  <script>
-  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
+<script>
+import { IonDatetime, IonDatetimeButton, IonModal, IonSelect, IonSelectOption, IonTextarea, IonItem, IonLabel, IonInput, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRadio, IonRadioGroup } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
     name: 'HomePage',
     components: {
-      IonContent,
-      IonHeader,
-      IonPage,
-      IonTitle,
-      IonToolbar
+        IonDatetime, IonDatetimeButton, IonModal,
+        IonSelect, IonSelectOption,
+        IonContent,
+        IonHeader,
+        IonPage,
+        IonTitle,
+        IonToolbar,
+        IonLabel,
+        IonInput,
+        IonItem,
+        IonTextarea,
+        IonRadio,
+        IonRadioGroup
     },
-  });
-  </script>
+});
+</script>
   
-  <style scoped>
-  #container {
+<style scoped>
+#container {
     text-align: center;
-    
+
     position: absolute;
     left: 0;
     right: 0;
     top: 50%;
     transform: translateY(-50%);
-  }
-  
-  #container strong {
+}
+
+#container strong {
     font-size: 20px;
     line-height: 26px;
-  }
-  
-  #container p {
+}
+
+#container p {
     font-size: 16px;
     line-height: 22px;
-    
+
     color: #8c8c8c;
-    
+
     margin: 0;
-  }
-  
-  #container a {
+}
+
+#container a {
     text-decoration: none;
-  }
-  </style>
+}
+</style>
   
