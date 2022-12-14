7ccc041ca510b81457de52af167e2680ced2ecae
frankPIF <ubayfranklou@gmail.com>
2022-12-14 15:28:42 +0800

    Migrated to local database.

--- src/store/users.js
+++ src/store/users.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+
 const state = () => ({
   users: [],
   auth: {},
@@ -25,18 +27,18 @@ const mutations = {
 
 const actions = {
   async fetchUsers({commit}){
-    let res = await axios.get(`${process.env.VUE_APP_API_URL}/users.php`);
+    let res = await axios.get(`/users.php`);
 
     commit('setUsers', res.data);
   },
   async registration({commit}, payload){
-    let res = await axios.post(process.env.VUE_APP_API_URL +  '/new_user.php', payload);
+    let res = await axios.post('/new_user.php', payload);
 
     localStorage.setItem('auth', JSON.stringify(res.data.user[0]));
     commit('setAuth', res.data.user[0]);
   },
   async login({commit}, payload){
-    let res = await axios.post(process.env.VUE_APP_API_URL +  '/login.php', payload);
+    let res = await axios.post('/login.php', payload);
 
     if (res.data.users.length == 0)
       return res.data.users.length;
